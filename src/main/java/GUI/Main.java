5a8f67cdde73d19322522808f35b5d4045a0714e
alex de león véliz <alexdlveliz@hotmail.com>
2019-06-26 19:43:27 -0600

    inicio de nueva aplicación, con maven

---
+++ src/main/java/GUI/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import inicioSesion.Login;
+
+/**
+ *
+ * @author Nahomi
+ */
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Login login = Login.getInstancia();
+        login.setVisible(true);
+    }
+    public static void entrar()
+    {
+        Principal principal = Principal.getInstancia();
+        principal.setVisible(true);
+    }
+}
